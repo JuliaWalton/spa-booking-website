691f08bb2c4f1e6cc668d145109864568d57fcd8
JuliaWalton <134331524+JuliaWalton@users.noreply.github.com>
2023-12-18 22:49:15 -0600

    updated script

--- script.js
+++ script.js
@@ -369,15 +369,15 @@ const selects = [...formSteps[currentStep].querySelectorAll('select')];
 
         if (select.classList.contains('required')) { 
                 if (select.value === "none") {
-                    select.style.backgroundColor = "rgba(216, 56, 123, .5)";
                     showError(select, `Selecting a service is required`);
+                    select.style.backgroundColor = "rgba(216, 56, 123, .5)";
                     select.style.outline = "1px solid black"
                     validation = false;
                 } else if (selects.every(checksPass) === true) {
-                    validation = true;
                     showSuccess(select);
                     select.style.backgroundColor = "white";
                     select.style.outline = "none"
+                    validation = true;
                 }
         } else {
             validation = true;
@@ -409,9 +409,7 @@ const selects = [...formSteps[currentStep].querySelectorAll('select')];
                 const checksPass = (currentValue) => currentValue.parentElement.classList.contains('success');
                 // console.log(inputs.every(checksPass))
                 if (inputs.every(checksPass) === true) {
-                    incrementor = 1;
-                    currentStep += incrementor;
-                    showCurrentStep();
+                    validation = true;
                 }
             } else {
                 validation = true;
@@ -450,6 +448,12 @@ submitBtn.addEventListener('click', (e) => {
     validation = false;
     nextStep();
     update.innerText = emailInput.value;
+    
+    if (validation === true) {
+    incrementor = 1;
+    currentStep += incrementor;
+    showCurrentStep();
+    }
 })
 // end of booking functionality
 
@@ -461,7 +465,7 @@ const reviews = [
         "name": 'Mateo Urdiales',
         "job": 'product manager',
         "title": 'Great Service, Awesome Amenitites',
-        "img": 'images/man-red-shirt.jpg',
+        "img": '/images/man-red-shirt.jpg',
         "text": 'My wife and I jump to come here whenever we can find a babysitter. Their deep tissue massages make me feel like a new man. Don\'t disregard their haircut services either. Sophia always provides me a great cut with great conversation.',
     },
     {
@@ -469,7 +473,7 @@ const reviews = [
         "name": 'Sarah Forbes',
         "job": 'beauty blogger',
         "title": 'Frequent Visitor',
-        "img": 'images/woman-towel.jpg',
+        "img": '/images/woman-towel.jpg',
         "text":'The best spa in Dallas x10! I am a frequent local who comes just to get pampered! I always come for an 80-minute aromatherapy massage and the staff is fantastic at making me feel so relaxed and at ease. For your special occasions, be sure to book a celebrity facial with Chloe!' ,
     },
     {
@@ -477,7 +481,7 @@ const reviews = [
         "name": 'Anna Koto',
         "job": 'accountant',
         "title": 'Wonderful Experience',
-        "img": 'images/woman-yellow-jacket.jpg',
+        "img": '/images/woman-yellow-jacket.jpg',
         "text": 'Had a fabulous facial with Amanda! She was caring, gentle, and knowledgeable. Spa Selene is amazing! Friendly staff, great service selections, and the spa smell is soo good! Already scheduled my next appointment!',
     },
     {
@@ -485,7 +489,7 @@ const reviews = [
         "name": 'Will Anderson',
         "job": 'buisness owner',
         "title": 'Nice Relaxing Spa',
-        "img": 'images/man-white-shirt.jpg',
+        "img": '/images/man-white-shirt.jpg',
         "text": 'Will be back as a regular customer for sure! I had the 50-minute therapeutic massage with Kim. She gave an amazing massage. The pressure was on point, and she checked in with me a couple of times to make sure I was taken care of. ',
     }
 ];
